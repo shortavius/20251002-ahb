eea2d0d66b4e3d1014378d6a8ef51cbe88b27069
Jason Melton <jmelton@jasonmelton.com>
2025-09-30 19:10:23 -0400

    Add support for Modulino Button board

--- ahb-demo.ino
+++ ahb-demo.ino
@@ -1,13 +1,27 @@
 #include "src/hrtbt/hrtbt.h"
+#include "src/modulino-button/modulino-button.h"
 
 #include <Arduino.h>
+#include <Modulino.h>
 
 void setup() {
+  // Initialize the Modulino library
+  Modulino.begin();
+
   // Initialize the heart beat module
   hrtbt_cfg();
+
+  // Initialize the Modulino Button board
+  modulino_buttons_cfg();
 }
 
 void loop() {
   // Handle any actions that the heart beat module needs to do
   hrtbt_action();
+
+  // Get the current statue of the buttons on the Modulino board
+  unsigned char led_state = modulino_buttons_get_button_states();
+
+  // Set the Modulino board's LEDs based on the state of the buttons
+  modulino_buttons_set_leds(led_state);
 }
